114c5d6f1e53932eb284fee58936e74f25807005
YanethMejiaR <yaneth.mejia@ariadnacg.com>
2025-03-11 19:21:27 -0500

    Feature: Database connection

--- back-project/src/index.js
+++ back-project/src/index.js
@@ -1,1 +1,13 @@
-console.log('Test v5');
+const express = require("express");
+require("dotenv").config();
+const connectionDB = require("./config/database");
+
+const app = express();
+const port = process.env.PORT || 3005;
+
+// listen: 2 param puerto y funcion flecha
+app.listen(port, () => {
+  console.log(`Project running ${port}`);
+});
+
+connectionDB();
